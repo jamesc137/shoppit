986c6e08d20bfb30cf3cdeef018668283120720d
James <jamesc137@protonmail.com>
2021-07-05 22:53:04 -0700

    add Shops (belongs to vendor) to ActiveAdmin Dashbaord

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
   devise_for :vendors
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :shops
 end
