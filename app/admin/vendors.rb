80a45446af90e5d488442b283e4d08c8ac902005
James <jamesc137@protonmail.com>
2021-07-05 23:16:29 -0700

    add name to vendors table in order to show in AA dashboard on Shop edit/create the vendor name

--- app/admin/vendors.rb
+++ app/admin/vendors.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Vendor do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+  permit_params :name, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   #
   # or
   #
@@ -14,5 +14,4 @@ ActiveAdmin.register Vendor do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
